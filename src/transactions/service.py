341ced8098b735afae37a370520d353888662203
Ethan Crowson <95368619+ethancrowson@users.noreply.github.com>
2025-09-21 14:16:42 +0100

    Added upload_transactions and get_user_summary to service.

--- src/transactions/service.py
+++ src/transactions/service.py
@@ -4,3 +4,9 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 class TransactionService:
     def __init__(self, session:AsyncSession):
         self.session = session
+
+    async def upload_transactions(self):
+        return {"status": "ok"}
+
+    async def get_user_summary(self):
+        return {"status": "ok"}
