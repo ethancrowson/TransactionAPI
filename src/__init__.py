5febb1583dcd5b345db245ce23abde2ff3960515
Ethan Crowson <95368619+ethancrowson@users.noreply.github.com>
2025-09-21 13:51:02 +0100

    added lifespan to the app.

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,16 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("server is starting up.")
+    yield
+    print("server is shutting down.")
+
+app = FastAPI(
+    title="Transaction Service",
+    description="A simple web service for a transaction application.",
+    version="0.0.1",
+    lifespan=lifespan
+)
